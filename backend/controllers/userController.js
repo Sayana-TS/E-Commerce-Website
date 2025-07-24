28e2ec305769c59b86696717a0d230de9e52c1c8
Sayana TS <sayanatsudheesh@gmail.com>
2025-07-24 13:26:44 +0530

    userController and userModel created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+import Users from "../models/userModel.js";
+
+
+const registerUser = async () => {}
+
+const loginUser = async () => {}
+
+const logoutUser = async () => {}
+
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDb from './config/db.js'
+import userRoute from './routes/userRoutes.js'
 
 
 dotenv.config()
@@ -15,5 +16,8 @@ app.use(express.urlencoded({extended : true}))
 app.use(cors())
 
 const port = process.env.PORT
+
+
+app.use('/api/user', userRoute)
 
 app.listen(port, ()=> console.log('Server started successfully'))
