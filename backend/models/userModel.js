882feda8b10fe3e82daa345ca28f0f360f4ec3e4
Sayana TS <sayanatsudheesh@gmail.com>
2025-07-31 12:42:17 +0530

    Login and register ui completed

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 
 const userSchema = mongoose.Schema(
   {
@@ -10,6 +10,7 @@ const userSchema = mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     password: {
       type: String,
@@ -26,12 +27,91 @@ const userSchema = mongoose.Schema(
   }
 );
 
+const reviewSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Users",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Users",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-const Users = mongoose.model('users', userSchema)
+const Users = mongoose.model("users", userSchema);
+const Reviews = mongoose.model("reviews", reviewSchema);
+const Products = mongoose.Schema("products", productSchema)
 
-
-export default Users
+export { Users, Reviews, Products };
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import { Routes, Route } from "react-router-dom"
 import HomeScreen from "./screens/HomeScreen"
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
+import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from "./screens/RegisterScreen"
 
 function App() {
 
@@ -7,6 +11,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<HomeScreen/>} />
+        <Route path="/login" element={<LoginScreen/>} />
+        <Route path="/register" element={<RegisterScreen/>} />
       </Routes>
     </>
   )
